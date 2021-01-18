179452582a678818f0d4815a09c8b87483bd2e67
ADMINS <ADMINS@DESKTOP-QFR4487>
2021-01-18 11:18:12 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment4_EmplyeeSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
